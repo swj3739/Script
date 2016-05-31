51302fe1f70d29931894a28fab2ae9d9c12132d6
swj3739 <swj1718@Naver.com>
2016-05-31 12:48:04 +0900

    중간발표준비
    중간발표준비

--- 파이썬텀프/launcher.py
+++ 파이썬텀프/launcher.py
@@ -3,20 +3,21 @@ from internetcountry import *
 
 #### Menu  implementation
 def printMenu():
-    print("\nWelcome! Book Manager Program (xml version)")
+    print("\nWelcome! Country Manager Program (xml version)")
     print("========Menu==========")
-    print("Load xml:  l")
-    print("Print dom to xml: p")
-    print("Quit program:   q")
-    print("print Country list: b")
+    print("xml 파일 로드:  l")
+    print("xml 파일 모두 출력: p")
+
+    print("나라이름 모두 출력: b")
     #print("Add new book: a")
-    print("SearchCountry Title: e")
-    print("SortByGround: s")
+    print("나라정보 검색: e")
+    print("땅 크기 정렬: s")
     #print("Make html: m")
-    print("----------------------------------------")
-    print("Get book data from isbn: g")
-    print("send maIl : i")
-    print("sTart Web Service: t")
+    #print("----------------------------------------")
+    # print("Get book data from isbn: g")
+   # print("send maIl : i")
+   # print("sTart Web Service: t")
+    print("프로그램 종료:   q")
     print("========Menu==========")
     
 def launcherFunction(menu):
@@ -29,7 +30,7 @@ def launcherFunction(menu):
     elif menu == 'b':
         PrintCountryList(["countryName",])
     elif menu == 'e':
-        keyword = str(input ('input keyword to search :'))
+        keyword = str(input ('나라 이름 :'))
         printBookList(SearchCountryName(keyword))
     elif menu == 'g': 
         isbn = str(input ('input isbn to get :'))
